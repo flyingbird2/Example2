9e7643927eb0baae11ff36fcf9d3a97d8148a052
flyingbird2 <buhui.08@163.com>
2020-11-15 20:26:53 +0800

    添加注释

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 void swap(int *x, int *y) {
     int t = 0;
+    //就是将地址里面的值取出来
     t = *x;
     *x = *y;
     *y = t;
@@ -10,6 +11,7 @@ void swap(int *x, int *y) {
 int main() {
     int a = 1;
     int b = 42;
+    //&是获取地址的意思
     swap(&a, &b);
     std::cout << "Hello, QHF World!" << std::endl;
     return 0;
