1a149ff0455b09f8f134758e398f42d18c5936bb
flyingbird2 <buhui.08@163.com>
2020-11-16 21:25:49 +0800

    布尔函数处理

--- main.cpp
+++ main.cpp
@@ -8,11 +8,23 @@ void swap(int *x, int *y) {
     *y = t;
 }
 
-int main() {
+
+/*int main() {
     int a = 1;
     int b = 42;
     //&是获取地址的意思
     swap(&a, &b);
     std::cout << "Hello, QHF World!" << std::endl;
+    return 0;
+}*/
+
+int main() {
+    bool isAlpha;
+    isAlpha = false;
+    if (!isAlpha) {
+        std::cout << "isAlpha=" << isAlpha << std::endl;
+        std::cout << std::boolalpha <<
+                  "isAlpha = " << isAlpha << std::endl;
+    }
     return 0;
 }
